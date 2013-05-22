6f8c1a46cc878b88fd5aa432a2bda030e9c990c3
freeyoung <ourbaozi@qq.com>
2013-05-23 02:10:19 +0800

    update vpn.sh

--- vpn.sh
+++ vpn.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
-route add -net 118.144.67.0/24 10.0.0.100
-route delete default
-route add default 10.8.10.209
+opr=$1
+
+[ "$opr" == "" ] && exit 2
+
+if [ "$opr" == "on" ]; then
+
+    route add -net 118.144.67.0/24 10.0.0.100
+    route delete default
+    route add default 10.8.10.209
+    
+elif [ "$opr" == "off" ]; then
+
+    route delete -net 118.144.67.0/24
+    route delete default
+    route add default 10.0.0.100
+
+fi
