9a0dd0a3372b0ce7d540b98c28136c6c22dd2b59
freeyoung <ourbaozi@qq.com>
2013-05-30 02:18:40 +0800

    use oh-my-zsh to replace grml

--- init.sh
+++ init.sh
@@ -1,8 +1,15 @@
-#!/bin/sh
+#!/bin/bash
+
+# oh-my-zsh
+curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
+ln -sf ~/.vim/zshrc.oh-my-zsh ~/.zshrc
+ln -sf ~/.vim/zshrc.extras ~/.zshrc.extras
+
+# tmux
+ln -sf ~/.vim/tmux.conf ~/.tmux.conf
+
+# vim
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 ln -sf ~/.vim/vimrc ~/.vimrc
-ln -sf ~/.vim/zshrc ~/.zshrc
-ln -sf ~/.vim/zshrc.local ~/.zshrc.local
-ln -sf ~/.vim/tmux.conf ~/.tmux.conf
 #echo "" > /etc/vim/vimrc
 vim -c "execute \"BundleInstall\" | q | q"
