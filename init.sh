bde7782eb198302b1b1932e92e0c4edcc086ef73
freeyoung <ourbaozi@qq.com>
2015-06-19 17:48:25 +0200

    Add the check for installation of zsh, and remove bashisms

--- init.sh
+++ init.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Check if zsh is already installed
+if ! which zsh >/dev/null 2>&1 ; then
+    echo "You need to install zsh first."
+    exit 2
+fi
+
 # tmux
 ln -sf ~/.vim/tmux.conf ~/.tmux.conf
 
@@ -8,9 +14,10 @@ if [ -d ~/.vim/bundle/vundle ]; then
     rm -rf ~/.vim/bundle/vundle
 fi
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
-unlink ~/.vimrc
+rm -f  ~/.vimrc
+
 echo "source ~/.vim/bundles.vim" > ~/.vimrc
-#echo "" > /etc/vim/vimrc
+
 vim -c "execute \"BundleInstall!\" | q | q"
 ln -sf ~/.vim/vimrc ~/.vimrc
 mkdir -p ~/.vim/undodir
@@ -19,10 +26,11 @@ mkdir -p ~/.vim/undodir
 if [ -d ~/.oh-my-zsh ]; then
     mv -f ~/.oh-my-zsh ~/.oh-my-zsh.bak
 fi
+
 git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 ln -sf ~/.vim/zshrc.oh-my-zsh ~/.zshrc
 ln -sf ~/.vim/zshrc.extras ~/.zshrc.extras
 
 chsh -s `which zsh`
 /usr/bin/env zsh
-source ~/.zshrc
+. ~/.zshrc
